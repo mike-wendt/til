60ff917ec0ec00c81ee848f4ee4d59a59fc70f94
Mike Wendt <1915404+mike-wendt@users.noreply.github.com>
2026-02-18 15:39:10 -0500

    FIX Remove antigen from backup and log exclude

--- dotfiles/0sync.sh
+++ dotfiles/0sync.sh
@@ -4,4 +4,4 @@ TGTDIR=${1:-.}
 
 # Sync changes made in core files to this directory
 mkdir -p ${TGTDIR}
-rsync -aPh --exclude="*.log" ~/.zshrc ~/.zprofile ~/.vimrc ~/.p10k.zsh ~/.gitconfig ~/.gitignore_global ~/antigen.zsh ${TGTDIR}
+rsync -aPh ~/.zshrc ~/.zprofile ~/.vimrc ~/.p10k.zsh ~/.gitconfig ~/.gitignore_global ${TGTDIR}
