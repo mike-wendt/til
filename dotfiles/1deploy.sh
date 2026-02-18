04190cb445747e93e3a2b1710720404aa31f5bb0
Mike Wendt <1915404+mike-wendt@users.noreply.github.com>
2026-02-18 16:29:57 -0500

    ENH Add OMZ, Antigen, Homebrew install option to deploy

--- dotfiles/1deploy.sh
+++ dotfiles/1deploy.sh
@@ -4,6 +4,17 @@ function logger {
   echo -e ">>>>\n>>>>${1}\n>>>>"
 }
 
+# Setup new machine with nothing installed
+function setup {
+  logger "Installing OMZ"
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  logger "Installing antigen..."
+  curl -L git.io/antigen > ~/antigen.zsh
+  logger "Installing homebrew"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  break
+}
+
 # Deploy these dotfiles and ZSH config to current computer
 function deploy {
   TS=`date +%Y%m%d-%H%M%S`
@@ -20,6 +31,15 @@ function deploy {
   logger "Go to https://github.com/romkatv/powerlevel10k?tab=readme-ov-file#fonts to install fonts"
   exit
 }
+
+# Check for new setup, install if confirmed
+echo "Should OMZ, Antigen & Homebrew be installed?"
+select yn in "Yes" "No"; do
+  case $yn in
+    Yes ) setup;;
+    No ) break;;
+  esac
+done
 
 # Check that we want to overwrite, always backup first
 echo "Are you sure you want to deploy these files over the existing install?"
