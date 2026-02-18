f2db867a5c4f3ac653dbac41fd01a87615bb0809
Mike Wendt <1915404+mike-wendt@users.noreply.github.com>
2026-02-18 15:29:27 -0500

    ENH Skip log files for backup

--- dotfiles/0sync.sh
+++ dotfiles/0sync.sh
@@ -4,4 +4,4 @@ TGTDIR=${1:-.}
 
 # Sync changes made in core files to this directory
 mkdir -p ${TGTDIR}
-rsync -aPh ~/.zshrc ~/.zprofile ~/.vimrc ~/.p10k.zsh ~/.gitconfig ~/.gitignore_global ~/.oh-my-zsh ~/.antigen ~/antigen.zsh ${TGTDIR}
+rsync -aPh --exclude="*.log" ~/.zshrc ~/.zprofile ~/.vimrc ~/.p10k.zsh ~/.gitconfig ~/.gitignore_global ~/.oh-my-zsh ~/.antigen ~/antigen.zsh ${TGTDIR}
